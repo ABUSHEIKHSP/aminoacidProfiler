86136dc9a4dfd174377ad24570b2d2116cc17735
ABUSHEIKHSP <abusheikh.s.p@gmail.com>
2022-11-08 13:40:23 +0530

    requirements added

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
-#!/usr/bin/python3
 
 import os
 
-os.system('pip3 install tk')
-os.system('pip3 install pytz')
-os.system('pip3 install pyinstaller')
-#os.system('sudo apt install git')
-os.system('pyinstaller --onefile -w aaP.py')
-
+#Getting username and dir path:
 user = os.path.expanduser('~')
 path = os.getcwd()
 
+#Installing necessary requiremnts:
+try:
+    os.sysconf(f'pip install -r {path}/requirements.txt')
+except:
+    os.sysconf(f'pip3 install -r {path}/requirements.txt')
+ 
+#Building App:
+os.system('pyinstaller --onefile -w aaP.py')
 
+#Adding App to path
 file = open(f'{user}/.bashrc','a+')
 file.write('\n#Adding path for aminoacidProfiler:')
 file.write(f'\nexport PATH="{path}/dist/:$PATH"')
 file.close()
-
 #os.system(f'open {user}/.bashrc')
