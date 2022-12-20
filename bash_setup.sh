8115171b39475e2f6af2470ea16a97cab2ed1231
ABUSHEIKHSP <abusheikh.s.p@gmail.com>
2022-12-20 07:58:50 +0530

    python3 and pip

---
+++ bash_setup.sh
@@ -0,0 +1,10 @@
+
+# Install python3 if it's not installed yet
+sudo apt install python3
+
+# Installing pip3
+sudo apt install python3-pip
+
+# Installing other requirements for python
+python3 setup.py
+
