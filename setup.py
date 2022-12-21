094098cebbac8c08939268f9a89dfb9a78000bac
ABUSHEIKHSP <abusheikh.s.p@gmail.com>
2022-12-21 09:42:33 +0530

    pip

--- setup.py
+++ setup.py
@@ -6,24 +6,17 @@ user = os.path.expanduser('~')
 path = os.getcwd()
 print(path)
 
-#Checking for pip:
-try:
-    subprocess.run(['pip3 install -r', 'requirements.txt'], check = True)
-except subprocess.CalledProcessError:
-    subprocess.run(['python3', 'get-pip.py --isolated'], check = True)
-    
-'''    
+#Installing pip:
+os.system('python3 get-pip.py --isolated')
+   
 #Installing necessary requiremnts:
 try:
     os.system(f'pip3 install -r {path}/requirements.txt')
 except:
-    os.system(f'pip install -r {path}/requirements.txt')'''
-
-
+    os.system(f'pip install -r {path}/requirements.txt')
  
 #Building App:
 os.system('pyinstaller --onefile -w aaP.py')
-
 
 #Adding App to path
 file = open(f'{user}/.bashrc','a+')
